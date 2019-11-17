2fa7b60af78d262909d191e54470323516042b81
Marco Cinus <cinus.marco@gmail.com>
2019-11-17 23:32:04 +0100

    Solved Kata Mumbling

---
+++ src/main/java/ch/cinus/kata/seven_kyu/mumbling/Accumul.java
@@ -0,0 +1,14 @@
+package ch.cinus.kata.seven_kyu.mumbling;
+
+import java.util.stream.Collectors;
+
+public class Accumul {
+
+  public static String accum(String s) {
+    final int[] i = {0};
+    return s.codePoints()
+        .mapToObj(c -> String.valueOf((char) c))
+        .map(s1 -> s1.toUpperCase() + s1.toLowerCase().repeat(i[0]++))
+        .collect(Collectors.joining("-"));
+  }
+}
---
+++ src/test/java/ch/cinus/kata/seven_kyu/mumbling/AccumulTest.java
@@ -0,0 +1,37 @@
+package ch.cinus.kata.seven_kyu.mumbling;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class AccumulTest {
+
+  @ParameterizedTest(name = "{index} {0}")
+  @MethodSource({"stringProvider"})
+  void test(String input, String expected) {
+    assertThat(Accumul.accum(input), equalTo(expected));
+  }
+
+  private static Stream<Arguments> stringProvider() {
+    return Stream.of(
+        Arguments.of(
+            "ZpglnRxqenU",
+            "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"),
+        Arguments.of(
+            "NyffsGeyylB",
+            "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"),
+        Arguments.of(
+            "MjtkuBovqrU",
+            "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"),
+        Arguments.of(
+            "EvidjUnokmM",
+            "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"),
+        Arguments.of(
+            "HbideVbxncC",
+            "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"));
+  }
+}
