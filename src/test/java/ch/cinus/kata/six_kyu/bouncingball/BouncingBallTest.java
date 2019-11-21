f66b176ac08141e2863f2b3f3c3b4b9be86e4144
Marco Cinus <cinus.marco@gmail.com>
2019-11-21 21:34:27 +0100

    Solved kata BouncingBall

---
+++ src/test/java/ch/cinus/kata/six_kyu/bouncingball/BouncingBallTest.java
@@ -0,0 +1,33 @@
+package ch.cinus.kata.six_kyu.bouncingball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BouncingBallTest {
+
+  @Test
+  void test1() {
+    assertEquals(3, BouncingBall.bouncingBall(3.0, 0.66, 1.5));
+  }
+
+  @Test
+  void test2() {
+    assertEquals(15, BouncingBall.bouncingBall(30.0, 0.66, 1.5));
+  }
+
+  @Test
+  void testInvalidParameters() {
+    assertEquals(-1, BouncingBall.bouncingBall(1, 0.66, 1.5));
+  }
+
+  @Test
+  void testInvalidParameters2() {
+    assertEquals(-1, BouncingBall.bouncingBall(-1, 0.66, 1.5));
+  }
+
+  @Test
+  void testInvalidParameters3() {
+    assertEquals(-1, BouncingBall.bouncingBall(10, 1, 1.5));
+  }
+}
