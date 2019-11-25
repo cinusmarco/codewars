e4d2753784646e682975642474ff27633f9c1fb0
Marco Cinus <cinus.marco@gmail.com>
2019-11-25 22:12:24 +0100

    Solved Kata Joseph Survivor, see https://medium.com/@rrfd/explaining-the-josephus-algorithm-11d0c02e7212 for the mathematical solution

---
+++ src/main/java/ch/cinus/kata/fivekyu/JosephusSurvivor.java
@@ -0,0 +1,33 @@
+package ch.cinus.kata.fivekyu;
+
+public class JosephusSurvivor {
+  public static int josephusSurvivor(final int n, final int k) {
+    return n == 1 ? 1 : (k - 1 + josephusSurvivor(n - 1, k)) % n + 1;
+  }
+
+  /*
+  Example with N=5 and k = 2
+    [1 2 3 4 5]  <-- Initial array
+    remove k-th element and shift so that k+1 as at first index
+    [3 4 5 1]
+    remove k-th element and shift so that k+1 as at first index
+    [5 1 3]
+    remove k-th element and shift so that k+1 as at first index
+    [3 5]
+    remove k-th element and shift so that k+1 as at first index
+    [3]
+     # What position is our final survivor in?
+     1
+     1
+     3
+     1
+     3
+     # What are the results of our equation above?
+     # (k-1 + f(n - 1, k)) % n + 1
+     1
+     1 = (2 - 1 + 1) % 2 + 1
+     3 = (2 - 1 + 1) % 3 + 1
+     1 = (2 - 1 + 3) % 4 + 1
+     3 = (2 - 1 + 1) % 5 + 1
+  */
+}
---
+++ src/test/java/ch/cinus/kata/fivekyu/JosephusSurvivorTest.java
@@ -0,0 +1,30 @@
+package ch.cinus.kata.fivekyu;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class JosephusSurvivorTest {
+
+  @ParameterizedTest(name = "Testing where n = {0} and k = {1}")
+  @MethodSource("provider")
+  void josephusTest(final int n, final int k, final int expected) {
+    assertThat(JosephusSurvivor.josephusSurvivor(n, k), equalTo(expected));
+  }
+
+  private static Stream<Arguments> provider() {
+    return Stream.of(
+        Arguments.of(7, 3, 4),
+        Arguments.of(11, 19, 10),
+        Arguments.of(40, 3, 28),
+        Arguments.of(14, 2, 13),
+        Arguments.of(100, 1, 100),
+        Arguments.of(2, 6, 1),
+        Arguments.of(5, 300, 1),
+        Arguments.of(300, 300, 265));
+  }
+}
