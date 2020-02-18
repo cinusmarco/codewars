739e27c9e617865ef61ca4b10bc9ea9b89f22d21
Marco Cinus <cinus.marco@gmail.com>
2020-02-18 22:39:31 +0100

    Removed some prints and configured logger to avoid spamming in the console

--- src/test/java/ch/cinus/kata/sixkyu/thedeafratsofhamelin/DinglemouseTest.java
+++ src/test/java/ch/cinus/kata/sixkyu/thedeafratsofhamelin/DinglemouseTest.java
@@ -7,19 +7,16 @@ import org.junit.jupiter.api.Test;
 class DinglemouseTest {
   @Test
   void ex1() {
-    System.err.println("1");
     assertEquals(0, Dinglemouse.countDeafRats("~O~O~O~O P"));
   }
 
   @Test
   void ex2() {
-    System.err.println("2");
     assertEquals(1, Dinglemouse.countDeafRats("P O~ O~ ~O O~"));
   }
 
   @Test
   void ex3() {
-    System.err.println("3");
     assertEquals(2, Dinglemouse.countDeafRats("~O~O~O~OP~O~OO~"));
   }
 }
