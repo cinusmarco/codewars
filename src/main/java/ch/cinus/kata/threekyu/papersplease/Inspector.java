a53e988011c241f6dd68741b9697e1f43b8a5cfb
E519308 <marco.cinus@sbb.ch>
2020-04-30 22:44:37 +0200

    Working on kata 'Papers, please'

--- src/main/java/ch/cinus/kata/threekyu/papersplease/Inspector.java
+++ src/main/java/ch/cinus/kata/threekyu/papersplease/Inspector.java
@@ -16,16 +16,23 @@ import java.util.stream.Stream;
 
 public class Inspector {
 
-  private static final List<String> COUNTRIES =
-      List.of("Arstotzka", "Antegria", "Impor", "Kolechia", "Obristan", "Republia", "United Federation");
+  public static final String ARSTOTZKA = "Arstotzka";
 
-  private List<String> allowedEntrance;
-  private Map<String, List<String>> requiredDocuments = new HashMap<>();
+  private static final List<String> COUNTRIES =
+      List.of(ARSTOTZKA, "Antegria", "Impor", "Kolechia", "Obristan", "Republia", "United Federation");
+  public static final String ENTRY_DENIED_MISSING_REQUIRED_DOCUMENT = "Entry denied: missing required %s.";
+  public static final String FOREIGNERS = "Foreigners";
+  public static final String ENTRANTS = "Entrants";
+  public static final String CITIZENS = "Citizens of Arstotzka";
+
+  private final List<String> allowedEntrance = new ArrayList<>();
+  private final Map<String, List<String>> requiredDocuments = new HashMap<>();
+
   private String wantedCriminal;
 
   public void receiveBulletin(String bulletin) {
     System.err.println(bulletin);
-    allowedEntrance = extractAllowedEntrance(bulletin);
+    allowedEntrance.addAll(extractAllowedEntrance(bulletin));
     wantedCriminal = extractWantedCriminal(bulletin);
     extractRequiredDocuments(bulletin);
   }
@@ -36,56 +43,118 @@ public class Inspector {
 
     personMap.entrySet().stream().forEach(entry -> p.addDocument(entry.getKey(), entry.getValue()));
 
-    if (isWantedCriminal(p.getName())) {
+    if (isWantedCriminal(p.getNames())) {
       return "Detainment: Entrant is a wanted criminal.";
+    }
+    final String informationMismatch = checkInformationMismatch(p);
+    if (informationMismatch != null) return informationMismatch;
+
+    // check documents required
+    final List<String> requiredDocs = checkRequiredDocuments(p);
+    if (!requiredDocs.isEmpty()) {
+      return String.format(ENTRY_DENIED_MISSING_REQUIRED_DOCUMENT, requiredDocs.get(0).replace("_", " "));
+    }
+
+    // check if he can enter
+    if (!p.getNation().equals(ARSTOTZKA) && !allowedEntrance.contains(p.getNation())) {
+      return "Entry denied: citizen of banned nation.";
     }
 
     // check for expired documents
+    final String expiredDocument = checkExpiredDocuments(p);
+    if (expiredDocument != null) return expiredDocument;
+
+    return p.getNation().equals(ARSTOTZKA) ? "Glory to Arstotzka." : "Cause no trouble.";
+  }
+
+  private List<String> checkRequiredDocuments(Person p) {
+    final List<String> requiredDocs =
+        new ArrayList<>(requiredDocuments.getOrDefault(ENTRANTS, Collections.emptyList()));
+    requiredDocs.removeAll(p.documents().map(Person.Document::getType).collect(Collectors.toList()));
+    if (!requiredDocs.isEmpty()) {
+      return requiredDocs;
+    }
+    if (p.getNation().equals(ARSTOTZKA)) {
+      return checkRequiredDocumentsForCitizens(p);
+    } else {
+      return checkRequiredDocumentsForForeigner(p);
+    }
+  }
+
+  private String checkExpiredDocuments(Person p) {
     final String expiredDocument =
         p.documents()
             .filter(Person.Document::isExpired)
             .findFirst()
-            .map(document -> String.format("Entry denied: %s expired.", document.type))
+            .map(document -> String.format("Entry denied: %s expired.", document))
             .orElse("");
     if (!expiredDocument.isEmpty()) {
       return expiredDocument;
     }
+    return null;
+  }
 
+  private String checkInformationMismatch(Person p) {
     // check for mismatches inside the documents
-    if (p.documents().collect(Collectors.groupingBy(Person.Document::getId)).size() > 1) {
+    if (p.documents()
+            .filter(document -> document.getId() != null)
+            .collect(Collectors.groupingBy(Person.Document::getId))
+            .size()
+        > 1) {
       return "Detainment: ID number mismatch.";
     }
 
-    final List<String> requiredDocs = new ArrayList<>(requiredDocuments.get(p.getNation()));
+    if (p.documents()
+            .filter(document -> Objects.nonNull(document.getNation()))
+            .collect(Collectors.groupingBy(Person.Document::getNation))
+            .size()
+        > 1) {
+      return "Detainment: nationality mismatch.";
+    }
+
+    if (p.documents()
+            .filter(document -> Objects.nonNull(document.getName()))
+            .collect(Collectors.groupingBy(Person.Document::getName))
+            .size()
+        > 1) {
+      return "Detainment: name mismatch.";
+    }
+    return null;
+  }
+
+  private List<String> checkRequiredDocumentsForForeigner(Person p) {
+
+    final List<String> requiredDocs =
+        new ArrayList<>(requiredDocuments.getOrDefault(FOREIGNERS, Collections.emptyList()));
     requiredDocs.removeAll(p.documents().map(Person.Document::getType).collect(Collectors.toList()));
-    if (!requiredDocs.isEmpty()) {
-      return String.format("Entry denied: missing required %s.", requiredDocs.get(0));
-    }
 
-    String answer;
-    if (p.getNation().equals("Arstotzka")) {
-      answer = inspectLocal(p);
-    } else {
-      answer = inspectForeigner(p);
+    if (requiredDocs.contains(Person.Document.ACCESS_PERMIT)) {
+      if (p.documents().anyMatch(this::testAlternativeToAccessPermit)) {
+        return Collections.emptyList();
+      }
     }
-    return answer;
+    return requiredDocs;
   }
 
-  private String inspectForeigner(Person p) {
-    if (allowedEntrance.contains(p.getNation())) {
-      return "Cause no trouble.";
-    } else {
-      return "Entry denied: citizen of banned nation.";
-    }
+  private boolean isDocumentValid(Person.Document document) {
+    return true;
   }
 
-  private String inspectLocal(Person p) {
-
-    return "Glory to Arstotzka.";
+  private boolean testAlternativeToAccessPermit(Person.Document document) {
+    return (document instanceof Person.Document.AccessPermit)
+        || (document instanceof Person.Document.GrantOfAyslum)
+        || (document instanceof Person.Document.DiplomaticAutorization);
   }
 
-  private boolean isWantedCriminal(String name) {
-    return wantedCriminal != null && Objects.equals(name, wantedCriminal);
+  private List<String> checkRequiredDocumentsForCitizens(Person p) {
+    final List<String> requiredDocs =
+        new ArrayList<>(requiredDocuments.getOrDefault(CITIZENS, Collections.emptyList()));
+    requiredDocs.removeAll(p.documents().map(Person.Document::getType).collect(Collectors.toList()));
+    return requiredDocs;
+  }
+
+  private boolean isWantedCriminal(List<String> names) {
+    return wantedCriminal != null && names.stream().anyMatch(n -> Objects.equals(wantedCriminal, n));
   }
 
   private List<String> extractAllowedEntrance(String bulletin) {
@@ -98,21 +167,22 @@ public class Inspector {
   }
 
   private void extractRequiredDocuments(String bulletin) {
-    final Pattern pattern = Pattern.compile(".*(Entrants|Foreigners) require (passport|access permit)\\n?.*");
+    final Pattern pattern =
+        Pattern.compile(".*(Entrants|Foreigners|Citizens of Arstotzka) require (passport|access permit|ID card)\\n?.*");
     final Matcher matcher = pattern.matcher(bulletin);
     if (matcher.find()) {
-      String document = matcher.group(2);
+      String document = matcher.group(2).replace(" ", "_");
       String who = matcher.group(1);
 
-      if ("Entrants".equals(who)) {
-        COUNTRIES.stream()
-            .map(country -> requiredDocuments.computeIfAbsent(country, k -> new ArrayList<>()))
-            .forEach(docs -> docs.add(document));
-      } else if ("Foreigners".equals(who)) {
-        COUNTRIES.stream()
-            .filter(s -> !"Arstotzka".equals(s))
-            .map(country -> requiredDocuments.computeIfAbsent(country, k -> new ArrayList<>()))
-            .forEach(docs -> docs.add(document));
+      if (ENTRANTS.equals(who)) {
+        final List<String> entrants = requiredDocuments.computeIfAbsent(ENTRANTS, k -> new ArrayList<>());
+        entrants.add(document);
+      } else if (FOREIGNERS.equals(who)) {
+        final List<String> foreigners = requiredDocuments.computeIfAbsent(FOREIGNERS, k -> new ArrayList<>());
+        foreigners.add(document);
+      } else if (CITIZENS.equals(who)) {
+        final List<String> citizens = requiredDocuments.computeIfAbsent(CITIZENS, k -> new ArrayList<>());
+        citizens.add(document);
       }
     }
   }
@@ -126,30 +196,40 @@ public class Inspector {
 
 class Person {
 
-  private List<Document> documents = new ArrayList<>();
+  private final Map<String, Document> documents = new HashMap<>();
 
   void addDocument(String type, String documentString) {
     Document document = Document.parseDocument(type, documentString);
-    documents.add(document);
+    documents.put(type, document);
   }
 
-  public boolean hasInformationMismatch() {
-    return false;
-  }
-
-  String getName() {
-    return documents.get(0).getName();
+  List<String> getNames() {
+    return documents.values().stream().map(Document::getName).distinct().collect(Collectors.toList());
   }
 
   String getNation() {
-    return documents.get(0).getNation();
+    final List<String> nations =
+        documents.values().stream()
+            .filter(Objects::nonNull)
+            .map(Document::getNation)
+            .filter(Objects::nonNull)
+            .distinct()
+            .collect(Collectors.toList());
+    return nations.stream().findAny().orElse(null);
   }
 
   public Stream<Document> documents() {
-    return documents.stream();
+    return documents.values().stream();
   }
 
   abstract static class Document {
+    static final String PASSPORT = "passport";
+    static final String ACCESS_PERMIT = "access_permit";
+    static final String GRANT_OF_ASYLUM = "grant_of_asylum";
+    static final String WORK_PASS = "work_pass";
+    static final String DIPLOMATIC_AUTHORIZATION = "diplomatic_authorization";
+    static final String CERTIFICATE_OF_VACCINATION = "certificate_of_vaccination";
+    static final String ID_CARD = "ID_card";
     String type;
     String name;
     LocalDate exp;
@@ -166,15 +246,28 @@ class Person {
 
     public static Document parseDocument(String type, String content) {
       switch (type) {
-        case "passport":
+        case PASSPORT:
           return Passport.parse(content);
-        case "access_permit":
+        case ACCESS_PERMIT:
           return AccessPermit.parse(content);
-        case "grant_of_asylum":
+        case GRANT_OF_ASYLUM:
           return GrantOfAyslum.parse(content);
+        case WORK_PASS:
+          return WorkPass.parse(content);
+        case DIPLOMATIC_AUTHORIZATION:
+          return DiplomaticAutorization.parse(content);
+        case CERTIFICATE_OF_VACCINATION:
+          return CertificateOfVaccination.parse(content);
+        case ID_CARD:
+          return IDCard.parse(content);
         default:
           throw new IllegalArgumentException("Unkown type: " + type);
       }
+    }
+
+    @Override
+    public String toString() {
+      return type.replace("_", " ");
     }
 
     public String getType() {
@@ -186,7 +279,7 @@ class Person {
     }
 
     public boolean isExpired() {
-      return exp.isBefore(LocalDate.of(1982, 11, 22));
+      return Objects.nonNull(exp) && exp.isBefore(LocalDate.of(1982, 11, 22));
     }
 
     public String getId() {
@@ -230,41 +323,97 @@ class Person {
       return matcher.find() ? String.format("%s", matcher.group(1)) : null;
     }
 
-    private static class Passport extends Document {
+    static class Passport extends Document {
       private Date dob;
 
       private Passport(String content) {
-        super("passport", content);
+        super(PASSPORT, content);
       }
 
       public static Document parse(String content) {
-        Passport passport = new Passport(content);
-        return passport;
+        return new Passport(content);
       }
     }
 
-    private static class AccessPermit extends Document {
+    static class AccessPermit extends Document {
 
       protected AccessPermit(String content) {
-        super("access_permit", content);
+        super(ACCESS_PERMIT, content);
       }
 
       public static Document parse(String content) {
-        AccessPermit accessPermit = new AccessPermit(content);
-        return accessPermit;
+        return new AccessPermit(content);
       }
     }
 
-    private static class GrantOfAyslum extends Document {
+    static class GrantOfAyslum extends Document {
 
       protected GrantOfAyslum(String content) {
-        super("grant_of_asylum", content);
+        super(GRANT_OF_ASYLUM, content);
       }
 
       public static Document parse(String content) {
-        GrantOfAyslum grantOfAyslum = new GrantOfAyslum(content);
-        return grantOfAyslum;
+        return new GrantOfAyslum(content);
       }
+    }
+
+    static class WorkPass extends Document {
+
+      protected WorkPass(String content) {
+        super(WORK_PASS, content);
+      }
+
+      public static Document parse(String content) {
+        return new WorkPass(content);
+      }
+    }
+
+    static class DiplomaticAutorization extends Document {
+      protected DiplomaticAutorization(String content) {
+        super(DIPLOMATIC_AUTHORIZATION, content);
+      }
+
+      public static Document parse(String content) {
+        return new DiplomaticAutorization(content);
+      }
+    }
+
+    static class CertificateOfVaccination extends Document {
+      List<String> vaccines = new ArrayList<>();
+
+      protected CertificateOfVaccination(String content) {
+        super(CERTIFICATE_OF_VACCINATION, content);
+      }
+
+      public static Document parse(String content) {
+        return new CertificateOfVaccination(content);
+      }
+
+      boolean isVaccinated(String vaccine) {
+        return vaccines.contains(vaccine);
+      }
+    }
+
+    static class IDCard extends Document {
+      protected IDCard(String content) {
+        super(ID_CARD, content);
+      }
+
+      public static Document parse(String content) {
+        return new IDCard(content);
+      }
+    }
+  }
+
+  static class InformationMismatchException extends Exception {
+    private final String mismatchedField;
+
+    public InformationMismatchException(String mismatchedField) {
+      this.mismatchedField = mismatchedField;
+    }
+
+    public String getMismatchedField() {
+      return mismatchedField;
     }
   }
 }
--- src/test/java/ch/cinus/kata/threekyu/papersplease/InspectorTest.java
+++ src/test/java/ch/cinus/kata/threekyu/papersplease/InspectorTest.java
@@ -81,4 +81,297 @@ class InspectorTest {
 
     assertThat(inspector.inspect(ortiz)).isEqualTo("Entry denied: passport expired.");
   }
+
+  @Test
+  void incrementalTest() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Entrants require passport\n" + "Allow citizens of Arstotzka\n" + "Wanted by the State: Jan Jager");
+
+    // Put first checks here
+    final Map<String, String> ibrahimovic =
+        Map.of(
+            "passport",
+            "NATION: United Federation\n"
+                + "DOB: 1947.03.26\n"
+                + "SEX: F\n"
+                + "ISS: Korista City\n"
+                + "ID#: LXZ8T-BTRT4\n"
+                + "EXP: 1985.12.03\n"
+                + "NAME: Ibrahimovic, Ana");
+
+    assertThat(inspector.inspect(ibrahimovic)).isEqualTo("Entry denied: citizen of banned nation.");
+
+    final Map<String, String> jager =
+        Map.of(
+            "passport",
+            "NATION: Impor\n"
+                + "DOB: 1924.07.01\n"
+                + "SEX: M\n"
+                + "ISS: Tsunkeido\n"
+                + "ID#: WW3YZ-YB20W\n"
+                + "EXP: 1983.04.20\n"
+                + "NAME: Jager, Jan");
+
+    assertThat(inspector.inspect(jager)).isEqualTo("Detainment: Entrant is a wanted criminal.");
+
+    final Map<String, String> kowalska =
+        Map.of(
+            "passport",
+            "NATION: Antegria\n"
+                + "DOB: 1944.09.12\n"
+                + "SEX: M\n"
+                + "ISS: Glorian\n"
+                + "ID#: ROUPK-RD23I\n"
+                + "EXP: 1985.02.17\n"
+                + "NAME: Kowalska, Michael");
+
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+
+    assertThat(inspector.inspect(jager)).isEqualTo("Cause no trouble.");
+    assertThat(inspector.inspect(kowalska)).isEqualTo("Cause no trouble.");
+
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+
+    assertThat(inspector.inspect(kowalska)).isEqualTo("Entry denied: missing required access permit.");
+
+    final Map<String, String> weiss =
+        Map.of(
+            "work_pass",
+            "NAME: Weiss, Gregory\n" + "FIELD: Fine arts\n" + "EXP: 1984.10.20",
+            "passport",
+            "NATION: Republia\n"
+                + "DOB: 1915.09.23\n"
+                + "SEX: M\n"
+                + "ISS: Lesrenadi\n"
+                + "ID#: Y577R-JAN09\n"
+                + "EXP: 1985.01.29\n"
+                + "NAME: Weiss, Gregory",
+            "access_permit",
+            "NAME: Weiss, Gregory\n"
+                + "NATION: Republia\n"
+                + "ID#: Y577R-JAN09\n"
+                + "PURPOSE: WORK\n"
+                + "DURATION: 2 MONTHS\n"
+                + "HEIGHT: 174.0cm\n"
+                + "WEIGHT: 82.0kg\n"
+                + "EXP: 1985.01.12");
+
+    assertThat(inspector.inspect(weiss)).isEqualTo("Cause no trouble.");
+  }
+
+  @Test
+  void accessPermit_canBeAvoided_whenGrantOfAsylumOrDiplomatic() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+
+    final Map<String, String> muller =
+        Map.of(
+            "passport",
+            "NATION: Antegria\n"
+                + "DOB: 1927.10.30\n"
+                + "SEX: F\n"
+                + "ISS: Glorian\n"
+                + "ID#: Z3BP6-MVGL6\n"
+                + "EXP: 1985.01.10\n"
+                + "NAME: Muller, Kristina",
+            "grant_of_asylum",
+            "NAME: Muller, Kristina\n"
+                + "NATION: Antegria\n"
+                + "ID#: Z3BP6-MVGL6\n"
+                + "DOB: 1927.10.30\n"
+                + "HEIGHT: 157.0cm\n"
+                + "WEIGHT: 58.0kg\n"
+                + "EXP: 1983.11.29}");
+
+    assertThat(inspector.inspect(muller)).isEqualTo("Cause no trouble.");
+  }
+
+  @Test
+  void entrants_withMismatchInDocuments_shouldBeDetained() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+
+    final Map<String, String> stolichnaya =
+        Map.of(
+            "work_pass",
+            "NAME: Stolichnaya, Erika\n"
+                + "FIELD: Construction\n"
+                + "EXP: 1984.09.29, passport=NATION: Impor\n"
+                + "DOB: 1952.12.23\n"
+                + "SEX: F\n"
+                + "ISS: Haihan\n"
+                + "ID#: UULUP-LA689\n"
+                + "EXP: 1985.11.08\n"
+                + "NAME: Stolichnaya, Erika",
+            "access_permit",
+            "NAME: Stolichnaya, Erika\n"
+                + "NATION: Republia\n"
+                + "ID#: UULUP-LA689\n"
+                + "PURPOSE: WORK\n"
+                + "DURATION: 3 MONTHS\n"
+                + "HEIGHT: 146.0cm\n"
+                + "WEIGHT: 41.0kg\n"
+                + "EXP: 1983.10.17");
+
+    assertThat(inspector.inspect(stolichnaya)).isEqualTo("Detainment: nationality mismatch.");
+  }
+
+  @Test
+  void entrants_withDiplomaticAutorization_shouldBeAllowed_whenDAIsValid() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+    inspector.receiveBulletin(
+        "Citizens of Arstotzka require ID card\n"
+            + "Deny citizens of Obristan\n"
+            + "Wanted by the State: Anastasia Lindberg");
+
+    final Map<String, String> muller =
+        Map.of(
+            "passport",
+            "NATION: Impor\n"
+                + "DOB: 1925.02.15\n"
+                + "SEX: M\n"
+                + "ISS: Tsunkeido\n"
+                + "ID#: TLI5M-ROXXN\n"
+                + "EXP: 1983.03.04\n"
+                + "NAME: Muller, Giovanni",
+            "diplomatic_authorization",
+            "NATION: Impor\n" + "NAME: Muller, Giovanni\n" + "ID#: TLI5M-ROXXN\n" + "ACCESS: Arstotzka, Antegria");
+
+    assertThat(inspector.inspect(muller)).isEqualTo("Cause no trouble.");
+  }
+
+  @Test
+  void certificateOfVaccinationTest() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+    inspector.receiveBulletin(
+        "Citizens of Arstotzka require ID card\n"
+            + "Deny citizens of Obristan\n"
+            + "Citizens of Antegria, Impor, Kolechia require HPV vaccination\n"
+            + "Wanted by the State: Joanna Costa");
+
+    final Map<String, String> muller =
+        Map.of(
+            "passport",
+            "NATION: Impor\n"
+                + "DOB: 1960.01.21\n"
+                + "SEX: M\n"
+                + "ISS: Tsunkeido\n"
+                + "ID#: AKUNR-KW24K\n"
+                + "EXP: 1984.04.09\n"
+                + "NAME: Owsianka, Niel",
+            "access_permit",
+            "NAME: Owsianka, Niel\n"
+                + "NATION: Impor\n"
+                + "ID#: AKUNR-KW24K\n"
+                + "PURPOSE: WORK\n"
+                + "DURATION: 3 MONTHS\n"
+                + "HEIGHT: 180.0cm\n"
+                + "WEIGHT: 91.0kg\n"
+                + "EXP: 1982.06.01",
+            "certificate_of_vaccination",
+            "NAME: Owsianka, Niel\n" + "ID#: AKUNR-KW24K\n" + "VACCINES: tetanus, polio, cholera");
+
+    assertThat(inspector.inspect(muller)).isEqualTo("Entry denied: access permit expired.");
+  }
+
+  @Test
+  void citizen_require_idCard() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+    inspector.receiveBulletin(
+        "Citizens of Arstotzka require ID card\n" + "Deny citizens of Impor\n" + "Wanted by the State: Sergei Weiss");
+
+    final Map<String, String> muller =
+        Map.of(
+            "passport",
+            "NATION: Arstotzka\n"
+                + "DOB: 1920.08.26\n"
+                + "SEX: M\n"
+                + "ISS: Paradizna\n"
+                + "ID#: ZACYE-YP3PC\n"
+                + "EXP: 1985.05.14\n"
+                + "NAME: Jacobs, Hugo",
+            "ID_card",
+            "NAME: Jacobs, Hugo\n" + "DOB: 1920.08.26\n" + "HEIGHT: 163.0cm\n" + "WEIGHT: 66.0kg");
+
+    assertThat(inspector.inspect(muller)).isEqualTo("Glory to Arstotzka.");
+  }
+
+  @Test
+  void detainment_whenNameMismatch() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Entrants require passport\n" + "Allow citizens of Arstotzka\n" + "Wanted by the State: Zera Graham");
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+    inspector.receiveBulletin(
+        "Deny citizens of Impor\n"
+            + "Entrants require tetanus vaccination\n"
+            + "Workers require work pass\n"
+            + "Wanted by the State: Kristina Seczek");
+
+    final Map<String, String> chernovski =
+        Map.of(
+            "passport",
+            "NATION: Arstotzka\n"
+                + "DOB: 1914.04.21\n"
+                + "SEX: M\n"
+                + "ISS: East Grestin\n"
+                + "ID#: RSRHT-SNKSZ\n"
+                + "EXP: 1984.06.23\n"
+                + "NAME: Praskovic, Otto",
+            "ID_card",
+            "NAME: Chernovski, Giovanni\n" + "DOB: 1914.04.21\n" + "HEIGHT: 186.0cm\n" + "WEIGHT: 99.0kg",
+            "certificate_of_vaccination",
+            "NAME: Chernovski, Giovanni\n" + "ID#: RSRHT-SNKSZ\n" + "VACCINES: tetanus, polio, HPV");
+
+    assertThat(inspector.inspect(chernovski)).isEqualTo("Detainment: name mismatch.");
+  }
+
+  @Test
+  void entryDenied_missingPassport() {
+    Inspector inspector = new Inspector();
+    inspector.receiveBulletin(
+        "Entrants require passport\n" + "Allow citizens of Arstotzka\n" + "Wanted by the State: Zera Graham");
+    inspector.receiveBulletin(
+        "Allow citizens of Antegria, Impor, Kolechia, Obristan, Republia, United Federation\n"
+            + "Wanted by the State: Ibrahim Spektor");
+    inspector.receiveBulletin("Foreigners require access permit\n" + "Wanted by the State: Erik Ibrahimovic");
+    inspector.receiveBulletin(
+        "Deny citizens of Impor\n"
+            + "Entrants require tetanus vaccination\n"
+            + "Workers require work pass\n"
+            + "Wanted by the State: Kristina Seczek");
+
+    final Map<String, String> yankov =
+        Map.of(
+            "ID_card",
+            "NAME: Yankov, Vasily\n" + "DOB: 1960.11.22\n" + "HEIGHT: 149.0cm\n" + "WEIGHT: 45.0kg",
+            "certificate_of_vaccination",
+            "NAME: Yankov, Vasily\n" + "ID#: GJ7L8-IBUET\n" + "VACCINES: HPV, tetanus, tuberculosis");
+
+    assertThat(inspector.inspect(yankov)).isEqualTo("Entry denied: missing required passport.");
+  }
 }
