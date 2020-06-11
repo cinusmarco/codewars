2ca5efa8331bcdf69e1725466c3974c524b1671e
E519308 <marco.cinus@sbb.ch>
2020-06-11 16:14:14 +0200

    Solved kata 'Prime Streaming (NC-17)'

--- src/main/java/ch/cinus/kata/twokyu/primestreamingnc17/Primes.java
+++ src/main/java/ch/cinus/kata/twokyu/primestreamingnc17/Primes.java
@@ -1,40 +1,43 @@
 package ch.cinus.kata.twokyu.primestreamingnc17;
 
-import java.util.Arrays;
+import java.util.BitSet;
 import java.util.stream.IntStream;
 
 public class Primes {
-
-  public static final int N = Integer.MAX_VALUE / 2;
+  private static final SieveOfEratosthenes SIEVE_OF_ERATOSTHENES = new SieveOfEratosthenes();
 
   public static IntStream stream() {
-    return sieveOfEratosthenes(N);
+    return IntStream.iterate(2, index -> SIEVE_OF_ERATOSTHENES.nextSetBit(index + 1));
+  }
+}
+
+class SieveOfEratosthenes {
+  private static final int N = 810000000;
+  private static final int FLOORED_SQRT_N = 28460;
+
+  private BitSet bitset;
+
+  SieveOfEratosthenes() {
+    init(); // assume everyone is prime
+    applySieve(); // filter out non primes
   }
 
-  static IntStream sieveOfEratosthenes(int n) {
-    boolean[] prime = new boolean[n + 1];
-    Arrays.fill(prime, true);
-    prime[0] = false;
-    prime[1] = false;
-    prime[4] = false;
-    prime[6] = false;
-    prime[8] = false;
-    prime[9] = false;
-    prime[10] = false;
-    prime[12] = false;
-    prime[14] = false;
-    prime[15] = false;
-    prime[16] = false;
-    prime[18] = false;
-    prime[20] = false;
+  private void init() {
+    bitset = new BitSet(N);
+    bitset.set(2, N - 1);
+  }
 
-    for (int p = 20; p * p <= n; p++) {
-      if (prime[p]) {
-        for (int i = p * 2; i <= n; i += p) {
-          prime[i] = false;
-        }
+  private void applySieve() {
+    int i = 1;
+    while (i < FLOORED_SQRT_N) {
+      i = bitset.nextSetBit(i + 1);
+      for (int j = i * i; j < N; j += i) {
+        bitset.clear(j);
       }
     }
-    return IntStream.range(2, n).filter(i -> prime[i]);
+  }
+
+  public int nextSetBit(int fromIndex) {
+    return bitset.nextSetBit(fromIndex);
   }
 }
--- src/test/java/ch/cinus/kata/twokyu/primestreamingnc17/PrimesTest.java
+++ src/test/java/ch/cinus/kata/twokyu/primestreamingnc17/PrimesTest.java
@@ -1,12 +1,12 @@
 package ch.cinus.kata.twokyu.primestreamingnc17;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import ch.cinus.kata.threekyu.primestreamingpg13.Primes;
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PrimesTest {
 
