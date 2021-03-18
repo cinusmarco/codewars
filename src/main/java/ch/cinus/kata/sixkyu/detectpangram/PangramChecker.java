9f70e40f2f90947835700eb138ff8c138ccce3e8
e519308 <marco.cinus@sbb.ch>
2021-03-18 10:34:12 +0100

    Some refactorings and alternative solutions to PangramChecker

--- src/main/java/ch/cinus/kata/sixkyu/detectpangram/PangramChecker.java
+++ src/main/java/ch/cinus/kata/sixkyu/detectpangram/PangramChecker.java
@@ -15,7 +15,12 @@ public class PangramChecker {
         == 26;
   }
 
-  public boolean check2(String sentence){
+  public boolean check2(String sentence) {
     return sentence.chars().map(Character::toLowerCase).filter(Character::isAlphabetic).distinct().count() == 26;
+  }
+
+  public boolean check3(String sentence) {
+    //solution by Andrea
+    return sentence.toLowerCase().replaceAll("[^a-z]", "").chars().distinct().count() == 26;
   }
 }
--- src/test/java/ch/cinus/kata/sixkyu/countingduplicates/CountingDuplicatesTest.java
+++ src/test/java/ch/cinus/kata/sixkyu/countingduplicates/CountingDuplicatesTest.java
@@ -1,12 +1,13 @@
 package ch.cinus.kata.sixkyu.countingduplicates;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CountingDuplicatesTest {
 
@@ -33,6 +34,7 @@ class CountingDuplicatesTest {
         Arguments.of("abcde", 0),
         Arguments.of("abcdea", 1),
         Arguments.of("indivisibility", 1),
-        Arguments.of("indivisibility", 1));
+        Arguments.of("aA11", 2),
+        Arguments.of("indivisibilities", 2));
   }
 }
